25fe0bbddb29960d8a0ad5afbcaec8df404c80bc
NikhilShingare007 <134536430+NikhilShingare007@users.noreply.github.com>
2024-09-04 23:03:22 +0530

    Create AtozTriangle.java

---
+++ Code Challenge/AtozTriangle.java
@@ -0,0 +1,20 @@
+package Allpattern;
+
+public class abcTriangle {
+    public static void main(String[] args) {
+        char ch ='A';
+        for(int i=0;i<6;i++) {
+            for(int j=0;j<6-i;j++) {
+                System.out.print(" ");
+            }
+
+            for(int k=0;k<=i;k++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+
+            System.out.println();
+        }
+
+    }
+}
